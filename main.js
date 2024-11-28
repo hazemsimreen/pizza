5de79c70d80a36e1d8f2b1668124b1c24aa35e64
hazemsimreen <hazemsimreen@gmail.com>
2024-11-28 20:54:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+async function  getpizza (){
+const response =  await fetch(`https://forkify-api.herokuapp.com/api/search?q=pizza`);
+const data =await response.json();
+const recipes= data.recipes;
+ 
+const result=recipes.map(function(recipe){
+
+return`  
+<div class "recipe">
+<h2>${recipe.title}</h2>
+<img src ='${recipe .image_url}'
+`;
+
+}). join("")
+
+document.querySelector(".recipes").innerHTML=result;
+
+}
+getpizza();
